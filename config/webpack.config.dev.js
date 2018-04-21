2f8316c044a2cd036682856b9c35c86e74678f5e
alexlrmn <lermana@post.bgu.ac.il>
2018-04-21 12:38:59 +0300

    added files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -164,7 +164,7 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
-                  moudle: true,
+                  module: true,
                   localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
               },
